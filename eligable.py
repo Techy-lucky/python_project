8ca0e84218926e0e3f892ea9a6f7f33cdfa0d096
Techy-lucky <lakshitverma98@gmail.com>
2026-09-11 09:54:47 +0530

    version 1.0.6

---
+++ eligable.py
@@ -0,0 +1,6 @@
+age = int(input("please enter the age : "))
+
+if(age>=18):
+    print("you are eligable for vote")
+else:
+    print("you are not eligable for vote")
---
+++ if_program.py
@@ -0,0 +1,14 @@
+# is_raining = True
+
+# if is_raining:
+#     print("raining outside")
+# else:
+#     print("not raining")
+
+
+is_raining = False
+
+if is_raining:
+    print("raining outside")
+else:
+    print("not raining")
